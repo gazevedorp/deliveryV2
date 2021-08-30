6b3630548a8c13f56e08cdb28fc2f7c5cef34394
Gabriel <gabrieldma27@gmail.com>
2021-08-29 23:40:21 -0300

    interface store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,6 +1,12 @@
 import create from 'zustand'
 
-export const useStore = create(set => ({
+interface storeState {
+  counter: number,
+  increasePopulation: any,
+  removeAllBears: any
+}
+
+export const useStore = create<storeState>(set => ({
   counter: 0,
   increasePopulation: () => set(state => ({ counter: state.counter + 1 })),
   removeAllBears: () => set({ counter: 0 })

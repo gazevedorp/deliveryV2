a0940acb77f84c17d7e954cae1cd4c0550995f00
Gabriel <gabrieldma27@gmail.com>
2021-08-29 23:49:44 -0300

    interface productItem

--- src/components/productItem.tsx
+++ src/components/productItem.tsx
@@ -5,19 +5,24 @@ import { useStore } from "../services/store";
 
 import { Card } from '../styles/components/productsItem'
 
-const ProductItem: React.FC = (props) => {
+interface steteProps {
+  name: string;
+  image: string;
+}
+
+const ProductItem: React.FC<steteProps> = (props) => {
 
   const increasePopulation = useStore(state => state.increasePopulation)
   const counter = useStore(state => state.counter)
-    
-    return (
-      <Card>
-          <p>{props.name}</p>
-          <img src={`${baseUrl}/${props.image}`} />
-          {counter}
-          <button onClick={increasePopulation}>Adicionar</button>
-      </Card>
-    )
+
+  return (
+    <Card>
+      <p>{props.name}</p>
+      <img src={`${baseUrl}/${props.image}`} />
+      {counter}
+      <button onClick={increasePopulation}>Adicionar</button>
+    </Card>
+  )
 }
 
 export default ProductItem
